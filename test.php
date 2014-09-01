5fa510620f27fb0f0158ecf77b7470962a07beb1
Justin Morris <justin@fuzioncore.com>
2014-08-31 22:38:37 -0700

    Update test.php

--- test.php
+++ test.php
@@ -2,8 +2,8 @@
 require_once 'shortcode.php';
 
 $test = new shortcode;
-//$test->reScramble();
-$x = rand(1500,2590746); //Target INT
+//$test->reScramble(); //This will echo out scramble vars for changing the scrambled alphabets.
+$x = rand(1500,2590746);
 $y = array(rand(100,1890),rand(5000,10000),rand(25000,250000));
 $ex = $test->encode_int($x);
 $ey = $test->encode_int($y);
